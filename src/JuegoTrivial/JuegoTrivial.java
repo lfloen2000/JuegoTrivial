865d9ee0d2355b6020de94f6ce044a8be51b9646
lfloen2000 <72011888+lfloen2000@users.noreply.github.com>
2020-10-30 00:17:12 +0100

    Add cambios

--- src/JuegoTrivial/JuegoTrivial.java
+++ src/JuegoTrivial/JuegoTrivial.java
@@ -1,24 +1,136 @@
 package JuegoTrivial;
 
+import javax.swing.JOptionPane;
+
 public class JuegoTrivial {
 
-	public static void main(String[] args) {
-		System.out.println("\n"
-				+ "  ____  _                           _     _   ____       \n"
-				+ " | __ )(_) ___ _ ____   _____ _ __ (_) __| | / __ \\ ___  \n"
-				+ " |  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ / _` / __| \n"
-				+ " | |_) | |  __/ | | \\ V /  __/ | | | | (_| | | (_| \\__ \\ \n"
-				+ " |____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\ \\__,_|___/ \n"
-				+ "                                             \\____/      \n"
-				+ "");
-		System.out.println("\n"
-				+ "  _____ ____  _____     _____    _    _        ____    _    __  __ _____ \n"
-				+ " |_   _|  _ \\|_ _\\ \\   / /_ _|  / \\  | |      / ___|  / \\  |  \\/  | ____|\n"
-				+ "   | | | |_) || | \\ \\ / / | |  / _ \\ | |     | |  _  / _ \\ | |\\/| |  _|  \n"
-				+ "   | | |  _ < | |  \\ V /  | | / ___ \\| |___  | |_| |/ ___ \\| |  | | |___ \n"
-				+ "   |_| |_| \\_\\___|  \\_/  |___/_/   \\_\\_____|  \\____/_/   \\_\\_|  |_|_____|\n"
-				+ "                                                                         \n"
-				+ "");
+	System.out.println("\n"
+			+ "  ____  _                           _     _   ____       \n"
+			+ " | __ )(_) ___ _ ____   _____ _ __ (_) __| | / __ \\ ___  \n"
+			+ " |  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ / _` / __| \n"
+			+ " | |_) | |  __/ | | \\ V /  __/ | | | | (_| | | (_| \\__ \\ \n"
+			+ " |____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\ \\__,_|___/ \n"
+			+ "                                             \\____/      \n"
+			+ "");
+	System.out.println("\n"
+			+ "  _____ ____  _____     _____    _    _        ____    _    __  __ _____ \n"
+			+ " |_   _|  _ \\|_ _\\ \\   / /_ _|  / \\  | |      / ___|  / \\  |  \\/  | ____|\n"
+			+ "   | | | |_) || | \\ \\ / / | |  / _ \\ | |     | |  _  / _ \\ | |\\/| |  _|  \n"
+			+ "   | | |  _ < | |  \\ V /  | | / ___ \\| |___  | |_| |/ ___ \\| |  | | |___ \n"
+			+ "   |_| |_| \\_\\___|  \\_/  |___/_/   \\_\\_____|  \\____/_/   \\_\\_|  |_|_____|\n"
+			+ "                                                                         \n"
+			+ "");
+	
+	
+	System.out.println("1.- ¿Cuál de las siguientes afirmaciones es incorrecta en relación al concepto de informática?");
+	System.out.println("a) En Latinoamérica, se utiliza como sinónimo la palabra computación, que proviene de cómputo (o cálculo).");
+	System.out.println("b) La informática es la ciencia que estudia el tratamiento automático de la información por medio de computadoras.");
+	System.out.println("c) La palabra «informática» del inglés informatique, derivado de la contracción de las palabras information y automatique, para dar idea de la automatización de la información");
+	System.out.println("d) Todas las afirmaciones anteriores son correctas");
+	int num1;
+	int num2 = 3; 
+	
+	String str = JOptionPane.showInputDialog("Introduce el número de la respuesta: ");
+	num1 = Integer.parseInt(str);
+	
+	if (num1==num2){
+		System.out.println("Acierto");
 	}
-
+	else {
+		System.out.println("Error");
+	}
+	
+	
+	
+	System.out.println("2.- En el origen de la programación están las tarjetas perforadas utilizadas en el siglo XIX en ...");
+	System.out.println("a) La información estadística");
+	System.out.println("b) La industria textil");
+	System.out.println("c) Los censos de población de Estados Unidos");
+	System.out.println("d) El desarrollo del ferrocarril");
+	
+	int num3;
+	int num4 = 2; 
+	
+	String str1 = JOptionPane.showInputDialog("Introduce un número entero: ");
+	num3 = Integer.parseInt(str1);
+	
+	if (num3==num4){
+		System.out.println("Acierto");
+	}
+	else {
+		System.out.println("Error");
+	}
+	
+	
+	System.out.println("3.- La parte esencial de dicho sistema operativo era el intérprete de comandos Hablamos de ...");
+	System.out.println("a) Windows");
+	System.out.println("b) Pc Shell");
+	System.out.println("c) Linux");
+	System.out.println("d) MS-DOS");
+	
+	int num5;
+	int num6 = 4; 
+	
+	String str2 = JOptionPane.showInputDialog("Introduce un número entero: ");
+	num5 = Integer.parseInt(str2);
+	
+	if (num5==num6){
+		System.out.println("Acierto");
+	}
+	else {
+		System.out.println("Error");
+	}
+	
+	
+	System.out.println("4.- Hardware dentro de un ordenador u otros dispositivos programables, que interpreta las instrucciones de un programa informático mediante la realización de las operaciones básicas aritméticas, lógicas y de entrada/salida del sistema.");
+	System.out.println("a) CPU");
+	System.out.println("b) Chip");
+	System.out.println("c) Bios");
+	System.out.println("d) Memoria");
+	
+	
+	
+	System.out.println("5.- Recientemente las Tecnologías de la Información y la Comunicación (TIC), están revolucionando especialmente al sector ...");
+	System.out.println("a) Industrial");
+	System.out.println("b) Educativo");
+	System.out.println("c) De la robótica");
+	System.out.println("d) Armamentístico");
+	
+	
+	System.out.println("6.- Sistema digital que transfiere datos entre los componentes de una computadora o entre varias computadoras. Está formado por cables o pistas en un circuito impreso, dispositivos como resistores y condensadores además de circuitos integrados.");
+	System.out.println("a) Sistema Operativo");
+	System.out.println("b) Chip");
+	System.out.println("c) Bus");
+	System.out.println("d) Zócalo");
+	
+	
+	System.out.println("7.- La icónica red social de mensajes cortos, Twitter, ha anunciado que aumentará el límite de los caracteres de los mensajes de la plataforma desde 140 a ...");
+	System.out.println("a) 150 caracteres");
+	System.out.println("b) 280 caracteres");
+	System.out.println("c) 180 caracteres");
+	System.out.println("d) 200 caracteres");
+	
+	
+	System.out.println("8.- ¿Cuál de los siguientes personajes relevantes de la historia de la informática terminó su formación universitaria?");
+	System.out.println("a) Bill Gates");
+	System.out.println("b) Steve Jobs");
+	System.out.println("c) Mark Zuckerberg");
+	System.out.println("d) Ninguno de ellos");
+	
+	
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	
+	
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
 }
+}
+
+
